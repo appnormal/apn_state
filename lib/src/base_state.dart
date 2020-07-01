d3b298644ae7a1598401ea7b6127c67f961d9a3d
Corina <corina@appnormal.com>
2020-07-01 09:16:06 +0200

    remove errorresponse from base state

--- lib/src/base_state.dart
+++ lib/src/base_state.dart
@@ -7,7 +7,7 @@ class BaseState extends ChangeNotifier {
   static T of<T>(BuildContext context, {bool listen = true}) => Provider.of<T>(context, listen: listen);
 
   ViewState _state = ViewState.Idle;
-  ErrorResponse error;
+  dynamic error;
 
   ViewState get state => _state;
 
@@ -36,11 +36,9 @@ class BaseState extends ChangeNotifier {
     try {
       setState(ViewState.Busy);
       return await apiCall();
-    } on ErrorResponse catch (errorResponse) {
-      error = errorResponse;
     } catch (e) {
       print(e);
-      error = ErrorResponse.fromMessage("unknown");
+      error = e;
     }
     setState(ViewState.Idle);
     return null;
@@ -54,27 +52,4 @@ abstract class BaseStateEvent<S> {
 
   /// Handle the event
   Future<void> handle();
-}
-
-// TODO ?
-class ErrorResponse {
-  int statusCode;
-  String message;
-
-  ErrorResponse({
-    this.statusCode,
-    this.message,
-  });
-
-  factory ErrorResponse.fromMessage(String message) => ErrorResponse(statusCode: 500, message: message);
-
-  factory ErrorResponse.fromJson(Map<String, dynamic> json) => new ErrorResponse(
-        statusCode: json["error"] != null && json["error"]["status_code"] != null ? json["error"]["status_code"] : null,
-        message: json["error"] != null && json["error"]["message"] != null ? json["error"]["message"] : null,
-      );
-
-  @override
-  String toString() {
-    return "ErrorResponse = $message";
-  }
 }
